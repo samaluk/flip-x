4dc8fc2cf37e56c458a8099d6630caeaca7165d8
Sebastián Maluk <samaluk@miuandes.cl>
2026-04-20 01:17:28 -0400

    feat(confect): bridge admin through spec tree

--- confect/spec.ts
+++ confect/spec.ts
@@ -1,5 +1,6 @@
 import { Spec } from "@confect/core";
 
+import { admin } from "./admin.spec";
 import { settings } from "./settings.spec";
 
-export default Spec.make().add(settings);
+export default Spec.make().add(admin).add(settings);
