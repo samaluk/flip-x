bb3dc0df689bab9972725870bcbbc5c170b3b5ce
Sebastián Maluk <samaluk@miuandes.cl>
2026-04-21 01:28:46 -0400

    feat(confect): migrate takeTurn mutation

--- confect/turns.spec.ts
+++ confect/turns.spec.ts
@@ -2,10 +2,17 @@ import { FunctionSpec, GroupSpec } from "@confect/core";
 import { Schema } from "effect";
 
 import { MatchSnapshot } from "./match-snapshot-schema";
-import * as turnFns from "./turns";
 import { SessionIdField } from "./session";
 
-const takeTurn = FunctionSpec.convexPublicMutation<typeof turnFns.takeTurn>()("takeTurn");
+const takeTurn = FunctionSpec.publicMutation({
+  name: "takeTurn",
+  args: Schema.Struct({
+    ...SessionIdField,
+    matchId: Schema.String,
+    action: Schema.Literal("hit", "stay"),
+  }),
+  returns: MatchSnapshot,
+});
 const resolveAction = FunctionSpec.publicMutation({
   name: "resolveAction",
   args: Schema.Struct({
