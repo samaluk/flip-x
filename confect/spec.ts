b4fa8d886fd29ae006afb461c1fab0aff84c6fdc
Sebastián Maluk <samaluk@miuandes.cl>
2026-04-21 01:22:13 -0400

    feat(confect): migrate resolveAction mutation

--- confect/spec.ts
+++ confect/spec.ts
@@ -4,5 +4,6 @@ import { admin } from "./admin.spec";
 import { matches } from "./matches.spec";
 import { rounds } from "./rounds.spec";
 import { settings } from "./settings.spec";
+import { turns } from "./turns.spec";
 
-export default Spec.make().add(admin).add(matches).add(rounds).add(settings);
+export default Spec.make().add(admin).add(matches).add(rounds).add(settings).add(turns);
